ffe9eb672635edbca071e5a1974fe5646d74d3d1
Jackie <jackieec956@gmail.com>
2025-11-19 23:09:53 -0500

    added last two functions

--- calculator.py
+++ calculator.py
@@ -1,5 +1,11 @@
 import math
 
+def square_root(a): 
+    if a < 0: 
+        raise ValueError
+    else:
+        math.sqrt(a)
+def hypotenuse(a, b): math.hypot(a, b) # can have negative nums
 def add(a,b): return a+b
 def subtract(a,b): return a-b
 def multiply(a, b): return a * b
@@ -12,3 +18,16 @@ def logarithm(a, b):
          raise ValueError 
     else: return math.log(b,a)# use math library/raise ValueError
 def exponent(a, b): return a**b
+def square_root(a):
+    try:
+        if a < 0:
+            raise ValueError("Cannot take square root of negative number")
+        return math.sqrt(a)
+    except ValueError:
+        raise   
+
+def hypotenuse(a, b):
+    try:
+        return math.hypot(a, b)  
+    except ValueError:
+        raise   
